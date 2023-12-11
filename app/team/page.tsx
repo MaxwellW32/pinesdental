89b6a710a0dda785952354a2727c4dc5d35e504b
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-11 06:01:42 -0500

    test

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import styles from "./page.module.css"
+import GridBoxes from '@/components/gridBoxes/GridBoxes'
+import { FillInData } from '@/useful functions/FillInData'
 
 export default function page() {
     return (
-        <div>page</div>
+        <main>
+            <section>
+                <h1>Out Team At Work</h1>
+
+                <GridBoxes>
+                    {FillInData.map((eachUser, eachUserIndex) => {
+                        return (
+                            <div key={eachUserIndex}>
+
+                            </div>
+                        )
+                    })}
+                </GridBoxes>
+            </section>
+        </main>
     )
 }
